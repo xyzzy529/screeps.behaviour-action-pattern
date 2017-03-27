0f8355aa9214bd755d2517ed9375b67e697e48bf
Spedwards <improbablepuzzle@gmail.com>
2017-03-28 00:25:07 +1100

    Got ahead of myself

--- creep.action.dropping.js
+++ creep.action.dropping.js
@@ -12,7 +12,7 @@ action.newTarget = function(creep) {
         drop = creep.pos.findClosestByRange(creep.room.structures.spawns);
     }
     if( !drop ) {
-        drop = creep.pos.findClosestByRange(creep.room.find(FIND_FLAGS, Flag.flagFilter(FLAG_COLOR.commands.drop)));
+        drop = creep.pos.findClosestByRange(creep.room.find(FIND_FLAGS, Flag.flagFilter(FLAG_COLOR.claim.spawn)));
     }
     return drop;
 };
