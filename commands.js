a05a5ecbf08e272420e4267c3aa17f4f7cfd346c
Paul Coughlin <pcoughlin@gmail.com>
2017-04-04 14:37:23 -0600

    Add manual command examples

--- commands.js
+++ commands.js
@@ -1,10 +1,9 @@
 // useful commands
 
-<<<<<<< Updated upstream
 console.log(JSON.stringify(Object) );
-=======
+
 Game.creeps['miner-250-3'].data.determinatedTarget ='source-mine-id'
->>>>>>> Stashed changes
+
 // Recycle a creep
 Creep.action.recycling.assign(Game.creeps['<creepName>']);
 
@@ -15,7 +14,7 @@ _.forEach(Memory.rooms, r => delete r.roadConstructionTrace);
 _.forEach(Game.constructionSites, s => s.remove());
 
 // spawn something...
-<<<<<<< Updated upstream
+Game.spawns['Spawn1'].createCreepBySetup(Creep.setup.upgrader);
 Game.spawns['Spawn1'].createCreepBySetup(Creep.setup.worker);
 // or
 Game.rooms['<roomName>'].spawnQueueLow.push({parts:[MOVE,WORK,CARRY],name:'max',setup:'worker'});
@@ -28,32 +27,24 @@ Game.spawns['Spawn1'].createCreep([MOVE,MOVE,WORK,CARRY,CARRY,CARRY,ATTACK,ATTAC
 // clear low priority queue
 Memory.rooms['W3S96'].spawnQueueLow = [0];
 // clear medium priority queue
-Memory.rooms['<roomName>'].spawnQueueMedium = [0];
+Memory.rooms['W3S96'].spawnQueueMedium = [0];
 // clear high priority queue
-Memory.rooms['<roomName>'].spawnQueueHigh = [0];
-=======
-Game.spawns['Spawn1'].createCreepBySetup(Creep.setup.miner);
-// or
-Game.rooms['<roomName>'].spawnQueueHigh.push({parts:[MOVE,CARRY],name:'max',setup:'worker'});
+Memory.rooms['W3S96'].spawnQueueHigh = [0];
+Memory.rooms['W3S96'].spawnQueueLow.length;
+Memory.rooms['W3S96'].spawnQueueMedium.length;
+Memory.rooms['W3S96'].spawnQueueHigh.length;
 
-// clear spawn queues for a room
-// clear low priority queue
-Memory.rooms['E81N77'].spawnQueueLow = [0];
-Memory.rooms['E81N77'].spawnQueueLow.length;
-// clear medium priority queue
-Memory.rooms['E81N77'].spawnQueueMedium = [0];
-Memory.rooms['E81N77'].spawnQueueMedium.length;
-// clear high priority queue
-Memory.rooms['E81N77'].spawnQueueHigh = [0];
-Memory.rooms['E81N77'].spawnQueueHigh.length;
->>>>>>> Stashed changes
-
+// length of spawnQueue's
 _myRoom = Memory.rooms['W3S96'];
 console.log(_myRoom.spawnQueueLow.length,
 _myRoom.spawnQueueMedium.length,
 _myRoom.spawnQueueHigh.length);
+
 // move Creep
-Game.creeps['miner-250-3'].move(LEFT);
+Game.creeps['<creepName>'].move(RIGHT);
+Game.creeps['upgrader-1000-2'].move(LEFT); // Temp for just 1 tick
+Game.creeps['upgrader-1000-2'].data.determinatedSpot.x = 10;
+Game.creeps['upgrader-1000-2'].data.determinatedSpot.y = 26;
 
 // Path moves (not persistant, must be issued each turn)
 Game.creeps['worker-800-2'].moveTo(29,31,{visualizePathStyle: {fill:'transparent',stroke:'#fff',lineStyle:'dashed',strokeWidth: .15,opacity: .1}});
@@ -62,14 +53,10 @@ Game.creeps['worker-800-2'].moveTo(29,31,{visualizePathStyle: {fill:'transparent
 Game.creeps['<creepName>'].data.creepType="recycler";
 Creep.action.recycling.assign(Game.creeps['<creepName>']);
 
-<<<<<<< Updated upstream
 // To override a module file create a copy of an existing module...
 // Name it "custom.<originalModuleName>" or "viral.<originalModuleName>".
 // see:
 // Then call this method (without ".js"):
-=======
-// To override a module file create a copy of an existing module and name it "custom.<originalModuleName>". Then call this method (without ".js"):
->>>>>>> Stashed changes
 getPath('<originalModuleName>', true);
 getPath('viral.creep.setup.upgrader', true);
 
