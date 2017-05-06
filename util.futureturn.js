c541ee07da18a9d92242ba3e22a77e38739fec43
Paul Coughlin <pcoughlin@gmail.com>
2017-05-06 08:15:58 -0600

    FutureTurn add cleanup

--- util.futureturn.js
+++ util.futureturn.js
@@ -20,16 +20,42 @@ module.exports = {
      * initialize the FutureTurn[]
      * @param {Boolean} [force=false] forces init to empty old commands
      */
-    init(force = false) {
+    init(clean = false, force = false) {
+        let cpuB4 = Game.cpu.getUsed();
         if (Memory.FutureTurn === undefined || force) {
             Memory.FutureTurn = [
                 {
-                    exeTick: 0,
+                    exeTick: Game.time,
                     evalCmd: "console.log('Started FutureCheck!');",
                     showLog: true, // show this message in console (default false)
+                    evalDone: false,
+                    evalError: null,
                 }
             ];
             Memory.FutureTurn.firstTick = Game.time;
+            Memory.FutureTurn.notify = 0;
+        }
+
+        if (clean) {
+            let original = Memory.FutureTurn.length;
+            let keptItems = [];
+            let tooOld = Game.time - 10000; // about 10 hours
+            for (let i in Memory.FutureTurn) {
+                FT = Memory.FutureTurn[i];
+                if ((!FT.evalDone || FT.evalError != '~') && FT.exeTick > tooOld) {
+                    keptItems[keptItems.length] = FT;
+                }
+            }
+            Memory.FutureTurn = keptItems;
+            let cpuDelta = (Game.cpu.getUsed() - cpuB4).toFixed(3);
+            let messageText = `FutureTurn cleanup original=${original} kept=${keptItems.length} &#916;=${keptItems.length - original} &#937;=${cpuDelta}`;
+            console.log(messageText);
+            if (keptItems.length > 150) {
+                Memory.FutureTurn.notify++;
+                Game.notify(`<h2>FutureTurn Cleanup</h2><br>${messageText}`, Memory.FutureTurn.notify);
+            } else {
+                Memory.FutureTurn.notify = 0;
+            }
         }
     },
     /**
@@ -38,13 +64,21 @@ module.exports = {
      * @param  {string} evalCmd command string that will be eval'ed at Tick
      * @return {number}         Index of inserted future command for use in subsequent calls
      */
-    cmd(exeTick, evalCmd, showLog = false) {
+    cmd(exeTick, evalCmd, showLog = true, turnCopys = 1) {
         let n = Memory.FutureTurn.length;
-        Memory.FutureTurn[n] = {
-            exeTick: Game.time + exeTick,
-            evalCmd: evalCmd,
-            showLog: showLog
-        };
+        let manualOffset = 0;
+        if (!Memory.my.inMain) {
+            manualOffset = 1;
+        }
+        for (let c = 0; c < turnCopys; c++) {
+            Memory.FutureTurn[n++] = {
+                exeTick: Game.time + (exeTick * c) + manualOffset,
+                evalCmd: evalCmd,
+                showLog: showLog,
+                evalDone: false,
+                evalError: null,
+            };
+        }
         //console.log(`****** FutureCmd added ${Memory.FutureTurn[n].exeTick} Cmd:${Memory.FutureTurn[n].evalCmd}`);
         //console.log('FutureTurn dump:'+JSON.stringify(Memory.FutureTurn) );
         return n;
@@ -74,14 +108,18 @@ module.exports = {
         //console.log(`FutureCheck for ${Game.time} length=${Memory.FutureTurn.length}`);
         let FT = Memory.FutureTurn;
         let allThisTick = Infinity; // used if AI is stopped, ONLY runs one Tick per pass.
-        for (let t = 0; t < FT.length; t++) {
-            if (FT[t].evalDone === undefined && (FT[t].exeTick == allThisTick || (allThisTick == Infinity && FT[t].exeTick <= Game.time))) {
+        let FTlength = FT.length;
+        for (let t = 0; t < FTlength; t++) {
+            if (!FT[t].evalDone && (FT[t].exeTick == allThisTick || (allThisTick == Infinity && FT[t].exeTick <= Game.time))) {
                 try {
                     eval(FT[t].evalCmd);
                     FT[t].evalDone = true;
+                    FT[t].evalError = '~'; // means to error thrown in eval()
                 } catch (e) {
-                    FT[t].evalDone = (e.stack || e.toString());
-                    let message = dye('FireBrick', `FutureCheck allThisTick ERROR FT[${t}] <br>` + (e.stack || e.toString()));
+                    FT[t].evalDone = true;
+                    FT[t].evalError = (e.stack || e.toString());
+                    let message = dye('FireBrick', `FutureTurn[${t}]: |${FT[t].evalCmd}|<br>`);
+                    message += dye('FireBrick', `FutureCheck allThisTick ERROR FT[${t}]<br>` + (e.stack || e.toString()));
                     console.log(message);
                 }
                 if (FT[t].showLog) {
